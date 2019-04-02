7dfd33eb6e1a81a3096e7061de21bbb6f435ca8b
Tak <thozumi.10@gmail.com>
2019-04-02 17:33:18 -0400

    Fix translate method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,7 +1,7 @@
 #write your code here
 def translate(word)
   if word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u'
-    return word[1..-1] + 'ay'
+    return word + 'ay'
   else
     return word[1..-1] + word[0] + 'ay'
   end
