3399e689b5b009c82ebf7961dcf7718d11b3f73d
Tak <thozumi.10@gmail.com>
2019-04-02 17:45:16 -0400

    Fix translate method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,13 +4,12 @@ def translate(phrase)
   pig_words = []
   words.each do |word|
     if word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u'
-      pig_words[-1] = word + 'ay'
+      pig_words[pig_words.length] = word + 'ay'
     elsif word[1] == 'a' || word[1] == 'e' || word[1] == 'i' || word[1] == 'o' || word[1] == 'u'
-      pig_words[-1] = word[1..-1] + word[0] + 'ay'
+      pig_words[pig_words.length] = word[1..-1] + word[0] + 'ay'
     else
-      pig_words[-1] = word[2..-1] + word[0..1] + 'ay'
+      pig_words[pig_words.length] = word[2..-1] + word[0..1] + 'ay'
     end
   end
-  pig_words.join(' ')
-  return pig_words
+  return pig_words.join(' ')
 end
