68088d1f6967a3ea0c82d0c3c5b4845ffb4e5c09
Tak <thozumi.10@gmail.com>
2019-04-02 17:31:53 -0400

    Fix translate method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,27 +1,7 @@
 #write your code here
 def translate(word)
-  vowels = ['a', 'e', 'i', 'o', 'u']
-  vowels.each do |l|
-    if word[0] == l
-      word += 'ay'
-      return word
-      break
-    end
-  end
-  vowels.each do |l|
-    if word[0] != l
-      word = word[1..-1] + word[0] + 'ay'
-      if word[0] == l
-        return word
-        break
-      end
-      vowels.each do |l|
-        if word[0] != l
-          word = word[1..-3] + word[0] + 'ay'
-        end
-      end
-      return word
-      break
-    end
-  end
+  if word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u'
+    return word[1..-1] + 'ay'
+  else
+    return word[1..-1] + word[0] + 'ay'
 end
