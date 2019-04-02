5d1e34a4c082d8d3df3f0c00c7bc2b3f714b591a
Tak <thozumi.10@gmail.com>
2019-04-02 17:11:34 -0400

    Fix translate method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,10 +4,6 @@ def translate(word)
   vowels.each do |l|
     if word[0] == l
       word += 'ay'
-    else
-      first = word[0]
-      word[0] = ''
-      word += first + 'ay'
     end
   end
   return word
