2b93c69af2c578274b67bb1f6c6b5962c43e1c75
Tak <thozumi.10@gmail.com>
2019-04-01 22:55:47 -0400

    created sum function

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,3 +6,13 @@ end
 def subtract(num1, num2)
   num1 - num2
 end
+
+def sum(numArray)
+  numLen = numArray.length
+  counter = 0
+  while counter < numLen
+    sum += numArray[counter]
+    counter += 1
+  end
+  return sum
+end
