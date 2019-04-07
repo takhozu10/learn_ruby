577cf476bb8f3935c246e8b0c6a26eb77ed2c8ff
Tak <thozumi.10@gmail.com>
2019-04-06 21:02:49 -0400

    Fixed translate method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -7,8 +7,10 @@ def translate(phrase)
       pig_words[pig_words.length] = word + 'ay'
     elsif word[1] == 'a' || word[1] == 'e' || word[1] == 'i' || word[1] == 'o' || word[1] == 'u'
       pig_words[pig_words.length] = word[1..-1] + word[0] + 'ay'
-    else
+    elsif word[2] == 'a' || word[2] == 'e' || word[2] == 'i' || word[2] == 'o' || word[2] == 'u'
       pig_words[pig_words.length] = word[2..-1] + word[0..1] + 'ay'
+    else
+      pig_words[pig_words.length] = word[3..-1] + word[0..2] + 'ay'
     end
   end
   return pig_words.join(' ')
