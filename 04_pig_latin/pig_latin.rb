470c147cf5c9880a2e403eaad716d655c68f6d86
Tak <thozumi.10@gmail.com>
2019-04-06 21:06:33 -0400

    Fixed translate method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,7 +3,9 @@ def translate(phrase)
   words = phrase.split(' ')
   pig_words = []
   words.each do |word|
-    if word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u'
+    if word[0..1] == 'qu'
+      pig_words[pig_words.length] = word[2..-1] + word[0..1] + 'ay'
+    elsif word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u'
       pig_words[pig_words.length] = word + 'ay'
     elsif word[1] == 'a' || word[1] == 'e' || word[1] == 'i' || word[1] == 'o' || word[1] == 'u'
       pig_words[pig_words.length] = word[1..-1] + word[0] + 'ay'
